0acea77d07a9d9b8bc09cfa9bb2873010f6c398b
Carol Dawson <carol_dawson1@hotmail.com>
2017-03-24 19:31:10 +0000

    Initialize markupcalculator based on input

--- markup_calculator.rb
+++ markup_calculator.rb
@@ -1,3 +1,9 @@
 class MarkupCalculator
   attr_reader :base_price, :worker_count, :materials
+  
+  def initialize(base_price, worker_count, materials)
+    @base_price = base_price
+    @worker_count = worker_count
+    @materials = materials
+  end
 end
--- spec/markup_calculator_spec.rb
+++ spec/markup_calculator_spec.rb
@@ -1,7 +1,26 @@
 require './markup_calculator'
 
 RSpec.describe MarkupCalculator do
+  subject(:calculator) { MarkupCalculator.new(base_price, worker_count, materials) }
+  let(:base_price) {1299.99}
+  let(:worker_count) {1}
+  let(:materials) {'food'}
+  
   it { is_expected.to respond_to(:base_price) }
   it { is_expected.to respond_to(:worker_count) }
   it { is_expected.to respond_to(:materials) }
+  
+  describe "initialize" do    
+    it "initializes with a base_price" do
+      expect(calculator.base_price).to eq(base_price)
+    end
+    
+    it "initializes with worker_count" do
+      expect(calculator.worker_count).to eq(worker_count)
+    end
+    
+    it "initializes with materials" do
+      expect(calculator.materials).to eq(materials)
+    end
+  end
 end
