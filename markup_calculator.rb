fd9dc04c821c215bbf03f15808bc1cecb2a25c07
Carol Dawson <carol_dawson1@hotmail.com>
2017-03-24 19:59:12 +0000

    Validate MarkupCalculator material type input to check it is alphabetical

--- markup_calculator.rb
+++ markup_calculator.rb
@@ -8,5 +8,10 @@ class MarkupCalculator
     
     raise ArgumentError.new("Base price must be numeric") unless @base_price.is_a?(Float)
     raise ArgumentError.new("Worker count must be an integer") unless @worker_count.is_a?(Integer)
+    raise ArgumentError.new("Materials must be alphabetical") unless is_alphabetical?(@materials)
+  end
+  
+  def is_alphabetical?(text)
+    text =~ /^[a-zA-Z]+$/
   end
 end
--- spec/markup_calculator_spec.rb
+++ spec/markup_calculator_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe MarkupCalculator do
   it { is_expected.to respond_to(:worker_count) }
   it { is_expected.to respond_to(:materials) }
   
-  describe "initialize" do    
+  describe "#initialize" do    
     it "initializes with a base_price" do
       expect(calculator.base_price).to eq(base_price)
     end
@@ -22,15 +22,33 @@ RSpec.describe MarkupCalculator do
     it "initializes with materials" do
       expect(calculator.materials).to eq(materials)
     end
+    
+    it "raises an error if base_price is not a number" do
+      base_price = 'Not a number'
+      expect { MarkupCalculator.new(base_price, worker_count, materials) }.to raise_error(ArgumentError)
+    end
+    
+    it "raises an error if worker_count is not an integer" do
+      worker_count = 'One'
+      expect { MarkupCalculator.new(base_price, worker_count, materials) }.to raise_error(ArgumentError)
+    end
+    
+    it "raises an error if materials is not all alphabetical" do
+      materials = 'One2'
+      expect { MarkupCalculator.new(base_price, worker_count, materials) }.to raise_error(ArgumentError)
+    end
+  end  
+  
+  describe "#is_alphabetical?" do
+    it { is_expected.to respond_to(:is_alphabetical?) }
+    
+    it "returns true for a alphabetical string" do
+      expect(subject.is_alphabetical?('abcdefg')).to be_truthy
+    end
+
+    it "returns false for a non alphabetical string" do
+      expect(subject.is_alphabetical?("1000.00")).to be_falsey
+    end
   end
   
-  it "raises an error if base_price is not a number" do
-    base_price = 'Not a number'
-    expect { MarkupCalculator.new(base_price, worker_count, materials) }.to raise_error(ArgumentError)
-  end
-  
-  it "raises an error if worker_count is not an integer" do
-    worker_count = 'One'
-    expect { MarkupCalculator.new(base_price, worker_count, materials) }.to raise_error(ArgumentError)
-  end
 end
