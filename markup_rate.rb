bb2457ddef706fc4295a122582ac492fd171c468
Carol Dawson <carol_dawson1@hotmail.com>
2017-03-25 13:14:40 +0000

    Add MarkupRate object with rate constants

---
+++ markup_rate.rb
@@ -0,0 +1,5 @@
+class MarkupRate
+  FLAT_RATE = 5.0
+  WORKER_RATE = 1.2
+  MATERIAL_RATE = { :drugs => 7.5, :food => 13.0, :electronics => 2.0 }
+end
---
+++ spec/markup_rate_spec.rb
@@ -0,0 +1,34 @@
+require './markup_rate'
+require 'spec_helper'
+
+RSpec.describe MarkupRate do
+  
+  describe "it has the required rates" do
+    
+    it "has a FLAT_RATE " do
+	expect(MarkupRate::FLAT_RATE).not_to be_nil
+    end 
+    
+    it "has a WORKER_RATE " do
+	expect(MarkupRate::WORKER_RATE).not_to be_nil
+    end 
+    
+    it "has a MATERIAL_RATE " do
+	expect(MarkupRate::MATERIAL_RATE).not_to be_nil
+    end 
+    
+    it "has a Pharmaceutical MATERIAL_RATE " do
+	expect(MarkupRate::MATERIAL_RATE[:drugs]).not_to be_nil
+    end 
+    
+    it "has an electronics MATERIAL_RATE " do
+	expect(MarkupRate::MATERIAL_RATE[:electronics]).not_to be_nil
+    end 
+    
+    it "has a food MATERIAL_RATE " do
+	expect(MarkupRate::MATERIAL_RATE[:food]).not_to be_nil
+    end 
+    
+  end
+  
+end
